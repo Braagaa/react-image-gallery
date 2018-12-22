e4d1ddb4c3e14aa72416168180ad6aca04dca77e
Michael Braga <michael_braga@outlook.com>
2018-12-22 02:35:54 -0500

    nav links successfully working

--- src/components/Feature/Item.js
+++ src/components/Feature/Item.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const FeatureItem = ({title, URL, key}) => (
     <li key={key} className="feature-list__item">
-        <a className="feature-list__item__img" 
+        <Link to={`/gallery/${title}/1`} className="feature-list__item__img" 
             style={{background: `url(${URL})`}}>
             {' '}
-        </a>
+        </Link>
         <h3 className="feature-list__item__title">{title}</h3>
     </li>
 );
--- src/components/Nav/Item.js
+++ src/components/Nav/Item.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import * as R from 'ramda';
 
 const getId = R.pipe(R.head, R.prop('id'));
 
 const Item = ({tags, photo}) => (
-    <li key={getId(photo)}><a href="www.example.com">{tags}</a></li>
+    <li key={getId(photo)}>
+        <Link to={`/gallery/${tags}/1`}>{tags}</Link>
+    </li>
 );
 
 export default Item;
